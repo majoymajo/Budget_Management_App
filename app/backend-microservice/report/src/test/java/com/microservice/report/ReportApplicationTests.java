b0f2f15bcdfc9a8a8b47a752ac3f4e44cfdd6f9f
Jacob <jacobowiz.7@protonmail.com>
2026-02-06 19:00:00 -0500

    fix changes test

--- app/backend-microservice/report/src/test/java/com/microservice/report/ReportApplicationTests.java
+++ app/backend-microservice/report/src/test/java/com/microservice/report/ReportApplicationTests.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = {
+                "spring.autoconfigure.exclude=" +
+                        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration"
+        })
 class ReportApplicationTests {
 
 	@Test
