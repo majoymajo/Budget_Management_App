d0a9a9b475150761d8ac65676b6c24c7f70b80b1
Jacob <jacobowiz.7@protonmail.com>
2026-02-11 21:18:42 -0500

    refactor: Outsource CORS configuration to yaml and separate the configuration class ReportApplication

---
+++ app/backend-microservice/report/src/main/java/com/microservice/report/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.microservice.report.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Value("${app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
+                .allowedMethods("GET", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
